e17ef5cae205323385d70cfd52e95cbc0ef75cc4
Selim Abdel Razek <srazek2003@hotmail.com>
2023-02-27 14:16:55 -0500

    ugh

--- p2exp1.py
+++ p2exp1.py
@@ -1,26 +1,40 @@
 import matplotlib.pyplot as plt
 
-from graph import is_connected, create_random_graph, has_cycle
+from graph import MVC, approx1, approx2, approx3, create_random_graph
 
-experiments = 1000
-nodes = 50
-edges = 50
+experiments = 100
+nodes = 8
+edges = 30
 
-chance_of_connection = []
-edge = []
+sumsMVC = []
+sumsA1 = []
+sumsA2 = []
+sumsA3 = []
+numEdges = []
 
-for j in range(1, edges):
-    connected = 0
+for j in range(1, edges, 5):
+
     for _ in range(experiments):
         G = create_random_graph(nodes, j)
-        if is_connected(G):
-            connected += 1
-    chance_of_connection.append(connected/experiments * 100)
-    edge.append(j)
 
+        sumsMVC.append(len(MVC(G)))
+        sumsA1.append(len(approx1(G)))
+        sumsA2.append(len(approx2(G)))
+        sumsA3.append(len(approx3(G)))
 
-plt.plot(edge, chance_of_connection, label = "Chance of Connection")
-plt.xlabel("Number of edges")
-plt.ylabel("Chance of connection in percentage")
+    numEdges.append(j)
+
+finalMVC = sum(sumsMVC)
+finalA1 = sum(sumsA1)
+finalA2 = sum(sumsA2)
+finalA3 = sum(sumsA3)
+#create a bar graph
+plt.bar(0, height=finalMVC, label = "MVC")
+plt.bar(1, height=finalA1, label = "Approximation 1")
+plt.bar(2, height=finalA2, label = "Approximation 2")
+plt.bar(3, height=finalA3, label = "Approximation 3")
+
+plt.ylabel = "MVC"
+plt.xlabel = "Approximations"
 plt.legend()
 plt.show()
