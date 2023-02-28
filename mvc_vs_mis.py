0fbed378a8df355f01e8bea3f2ba1758d6390292
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 23:12:01 -0500

    mvc vs mis

--- mvc_vs_mis.py
+++ mvc_vs_mis.py
@@ -2,9 +2,7 @@ from graph import MIS, MVC, create_random_graph
 
 def compare(n):
     G = create_random_graph(n, n-1)
-    print("length of MIS: " + str(len(MIS(G))))
-    print("length of MVC: " + str(len(MVC(G))))
+    print(f"for {n} vertices: length of MIS ({len(MIS(G))}) + length of MVC ({len(MVC(G))}) = {len(MIS(G)) + len(MVC(G))}")
 
 for i in range(2, 15):
-    print("for n = " + str(i))
     compare(i)
